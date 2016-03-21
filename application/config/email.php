7e33b78a1ad5a83a3979601d57b8378ff7a1e3ff
heru prambadi <heruprambadi@gmail.com>
2016-03-21 11:18:59 +0700

    HMVC module

---
+++ application/config/email.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$config["mailtype"] = "html";
+$config["protocol"] = "sendmail";
+$config["smtp_host"] = "ssl://smtp.googlemail.com";
+$config["smtp_user"] = "heruprambadi@gmail.com";
+$config["smtp_pass"] = "1213141516171819heru";
+$config["smtp_port"] = 465;
+$config["crlf"] = "\r\n";
+$config["newline"] = "\r\n";
